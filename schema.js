f12ce8e7aef377811f377cd4bb1fbb627bd9d166
andela-oolarewaju <olarewaju.oreoluwa@andela.com>
2015-10-16 17:24:16 +0100

    add schema

--- schema.js
+++ schema.js
@@ -1,56 +1,52 @@
-// get dependencies
+//require sequelize
 var Sequelize = require('sequelize');
 //sequelize initialization
 var sequelize = new Sequelize("postgres://andela:andela@localhost:5432/sequelize");
+//function to attach methods
+var Model = function() {}
+Model.prototype.User = sequelize.define('User', {
+  firstName: {
+    type: Sequelize.STRING,
+    primaryKey: true,
+    allowNull: false,
+    unique: true
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  }
+});
 
-var Model = function () {}
-	Model.prototype.User = sequelize.define('User', {
-		firstName: {
-			type: Sequelize.STRING,
-			primaryKey: true,
-			allowNull: false,
-			unique: true
-		},
-		lastName: {
-			type: Sequelize.STRING,
-			allowNull: false, 
-		}
-	});
+Model.prototype.Document = sequelize.define('Document', {
+  title: {
+    type: Sequelize.STRING,
+    unique: true,
+    primaryKey: true,
+    allowNull: false
+  },
+  publishDate: Sequelize.STRING
+});
 
-	Model.prototype.Document = sequelize.define('Document', {
-		title: {
-			type: Sequelize.STRING,
-			unique: true,
-			primaryKey: true,
-			allowNull: false
-		}
-	});
+Model.prototype.Role = sequelize.define('Role', {
+  title: {
+    type: Sequelize.STRING,
+    primaryKey: true,
+    unique: true,
+    allowNull: false
+  }
+});
+//associate document with role. creates a roleTile field in document table
+Model.prototype.Role.hasMany(Model.prototype.Document)
 
-	Model.prototype.Role = sequelize.define('Role', {
-		title: {
-			type: Sequelize.STRING,
-			primaryKey: true,
-			unique: true,
-			allowNull: false
-		}
-	});
-	//associate document with role
-	Model.prototype.Role.hasMany(Model.prototype.Document)
-	//associate document and user
-	Model.prototype.User.hasOne(Model.prototype.Document, {
-	  foreignKey: {
-	    allowNull: false
-	  }
-	});
-	//associate user with role
-	Model.prototype.Role.hasOne(Model.prototype.User,  {
-	  foreignKey: {
-	    allowNull: false
-	  }
-	});
+//associate user with role. create a roleTitle field in user table
+Model.prototype.Role.hasOne(Model.prototype.User, {
+  foreignKey: {
+    allowNull: false
+  }
+});
 
-	sequelize.sync().then(function() {
-		return [Model.prototype.User, Model.prototype.Document, Model.prototype.Role];
-	});
-	
+sequelize.sync().then(function() {
+  return [Model.prototype.User, Model.prototype.Document, Model.prototype.Role];
+});
+
 exports.Model = Model
