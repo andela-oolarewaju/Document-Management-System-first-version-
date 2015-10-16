8549aca7677b5366848da90a2710db2eacd4190f
andela-oolarewaju <olarewaju.oreoluwa@andela.com>
2015-10-16 17:23:59 +0100

    add documentManager for functions

--- documentManager.js
+++ documentManager.js
@@ -1,127 +1,127 @@
 var Sequelize = require('sequelize');
 var schema = require('./schema');
-//sequelize initialization
-var sequelize = new Sequelize("postgres://andela:andela@localhost:5432/firstVersion");
-var DocumentManager = function(){}
 
-DocumentManager.prototype.createUser = function(first, last, role) {
-  schema.Model.prototype.Role.findOrCreate({
-	  where : {
-		  title : role
-		}
-	}).then(function (){
-		return schema.Model.prototype.User.create({
-      firstName : first,
-      lastName: last,
-      RoleTitle: role
-		});	
-	}); 
-}
-
-DocumentManager.prototype.getAllUsers = function() {
-	var userLength = 0;
-  return schema.Model.prototype.User.findAll().then(function (users) {
-  	for (var key in users){
-    	if(users.hasOwnProperty(key)){
-    		++userLength;
-    	}
-    }
-    return userLength;
-  });
-};
-
-DocumentManager.prototype.createRole = function(title) {
-	var newRole = {
-    title : title
-  }
-  return schema.Model.prototype.Role.create(newRole).then(function () {
-      console.log(newRole)
-  });
-}
-
-DocumentManager.prototype.getAllRoles = function() {
-	var roleLength = 0
-  return schema.Model.prototype.Role.findAll().then(function (roles) {
-    for (var key in roles){
-    	if(roles.hasOwnProperty(key)){
-    		++roleLength;
-    	}
-    }
-    return roleLength;
-  });
-};
-
-DocumentManager.prototype.createDocument = function(title, roleTitle, userFirstName, userLastName ) {
-	schema.Model.prototype.Role.findOrCreate({
-	  where : {
-		  title : roleTitle
-		}
-	}).then(function(){
-		schema.Model.prototype.User.findOrCreate({
-			where : {
-				firstName : userFirstName,
-				lastName : userLastName,
-				RoleTitle : roleTitle
-			}
-		}).then(function() {
-			return schema.Model.prototype.Document.create({
-				title : title,
-				RoleTitle : roleTitle,
-				UserFirstName : userFirstName,
-			});
-		})
-	});
-}
-
-DocumentManager.prototype.getAllDocuments = function(limit) {
-	var allDocs = [];
-  return schema.Model.prototype.Document.findAll({
-  	limit : limit,
-  	order : '"updatedAt" DESC'
-  }).then(function (documents) {
-  	for(var key in documents) {
-      if(documents.hasOwnProperty(key)){
-      	allDocs.push(documents[key].dataValues);
+module.exports = {
+  //create user function
+  createUser: function(first, last, role) {
+    //find or create a role then create a user
+    return schema.Model.prototype.Role.findOrCreate({
+      where: {
+        title: role
       }
-    }
-     return allDocs; 
-  });
-};
-
-DocumentManager.prototype.getAllDocumentsByRole = function(role, limit) {
-	var allDocs = [];
-  return schema.Model.prototype.Document.findAll({ where : {
-  	RoleTitle : role
+    }).then(function() {
+      return schema.Model.prototype.User.create({
+        firstName: first,
+        lastName: last,
+        RoleTitle: role
+      });
+    });
   },
-  	limit : limit,
-  	order : '"updatedAt" DESC'
-  }).then(function (documents) {
-  	console.log("doc", documents);
-  	for(var key in documents) {
-      if(documents.hasOwnProperty(key)){
-      	allDocs.push(documents[key].dataValues);
+  //get all users
+  getAllUsers: function() {
+    var userLength = 0;
+    return schema.Model.prototype.User.findAll().then(function(users) {
+      //iterate through users
+      for (var key in users) {
+        if (users.hasOwnProperty(key)) {
+          //increment userLength if users has property key
+          ++userLength;
+        }
       }
-    }
-     return allDocs; 
-  });
-};
-
-DocumentManager.prototype.getAllDocumentsByDate = function(date, limit) {
-	var allDocs = [];
-  return schema.Model.prototype.Document.findAll({ where : {
-  	createdAt : date
+      return userLength;
+    });
   },
-  	limit : limit,
-  	order : '"createdAt" DESC'
-  }).then(function (documents) {
-  	for(var key in documents) {
-      if(documents.hasOwnProperty(key)){
-      	allDocs.push(documents[key].dataValues);
+  //find or create a role
+  createRole: function(title) {
+    return schema.Model.prototype.Role.findOrCreate({
+      where: {
+        title: title
       }
-    }
-      console.log("docs:", allDocs);
+    });
+  },
+  //get all roles
+  getAllRoles: function() {
+    var roleLength = 0
+    return schema.Model.prototype.Role.findAll().then(function(roles) {
+      //iterate through roles
+      for (var key in roles) {
+        if (roles.hasOwnProperty(key)) {
+          //increment roleLength if roles has property key
+          ++roleLength;
+        }
+      }
+      return roleLength;
+    });
+  },
+  //create a document
+  createDocument: function(title, roleTitle) {
+    //format date like dd-mm--yy
+    var currentDate = new Date();
+    currentDate = currentDate.getDay() + "-" + currentDate.getMonth() + "-" + currentDate.getFullYear();
+    //find or create then create a document and set publish date to current date
+    return schema.Model.prototype.Role.findOrCreate({
+      where: {
+        title: roleTitle
+      }
+    }).then(function() {
+      return schema.Model.prototype.Document.create({
+        title: title,
+        RoleTitle: roleTitle,
+        publishDate: currentDate
+      });
+    });
+  },
+  //get all documents
+  getAllDocuments: function(limit) {
+    var allDocs = [];
+    //get docs in descending order by limit
+    return schema.Model.prototype.Document.findAll({
+      limit: limit,
+      order: '"updatedAt" DESC'
+    }).then(function(documents) {
+      for (var key in documents) {
+        if (documents.hasOwnProperty(key)) {
+          allDocs.push(documents[key].dataValues);
+        }
+      }
       return allDocs;
-  });
-};
-
-exports.DocumentManager = DocumentManager;
+    });
+  },
+  //get docs by role
+  getAllDocumentsByRole: function(role, limit) {
+    var allDocs = [];
+    return schema.Model.prototype.Document.findAll({
+      where: {
+        RoleTitle: role
+      },
+      limit: limit,
+      order: '"updatedAt" DESC'
+    }).then(function(documents) {
+      for (var key in documents) {
+        if (documents.hasOwnProperty(key)) {
+          allDocs.push(documents[key].dataValues);
+        }
+      }
+      return allDocs;
+    });
+  },
+  //get all docs by date
+  getAllDocumentsByDate: function(date, limit) {
+    var allDocs = [];
+    //get docs created on the date passed by user
+    return schema.Model.prototype.Document.findAll({
+      where: {
+        publishDate: date
+      },
+      order: '"createdAt" DESC',
+      limit: limit
+    }).then(function(documents) {
+      for (var key in documents) {
+        if (documents.hasOwnProperty(key)) {
+          allDocs.push(documents[key].dataValues);
+        }
+      }
+      return allDocs;
+    });
+  }
+}
